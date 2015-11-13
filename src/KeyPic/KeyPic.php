b118e58e7a88eef7d755662799a643f2f27e3b16
zaid <zaid_r_86@hotmail.com>
2015-11-13 15:05:23 +0200

    refactoring main methods

--- src/KeyPic/KeyPic.php
+++ src/KeyPic/KeyPic.php
@@ -5,7 +5,8 @@ namespace Zrashwani\KeyPic;
 use Psr\Http\Message\ServerRequestInterface;
 use GuzzleHttp\Psr7;
 
-class KeyPic {
+class KeyPic
+{
 
     /*@var RequestInterface $request */
     private $request;
@@ -14,64 +15,101 @@ class KeyPic {
     private $UserAgent = 'Keypic PHP Class, Version: 2.1';
     private $SpamPercentage = 70;
     private $host = 'http://ws.keypic.com';
-    private $url = '/';
     private $FormID;
     private $PublisherID;
     private $Token;
     private $Debug;
+    private $TokenInputName;
 
-    private function __clone() {
-    }
 
-    public function __construct(ServerRequestInterface $request) {
+    public function __construct(ServerRequestInterface $request)
+    {
         $this->request = $request;
     }
 
-    public function getHost() {
+    public function getHost()
+    {
         return $this->host;
     }
 
-    public function setHost($host) {
-        $this->host = $host;
+    public function setHost($host)
+    {
+        $new = clone $this;
+        $new->host = $host;
+        return $new;
     }
 
-    public function getSpamPercentage() {
+    public function getSpamPercentage()
+    {
         return $this->SpamPercentage;
     }
 
-    public function setSpamPercentage($SpamPercentage) {
-        $this->SpamPercentage = $SpamPercentage;
+    public function setSpamPercentage($SpamPercentage)
+    {
+        $new = clone $this;
+        $new->SpamPercentage = $SpamPercentage;
+        return $new;
     }
 
-    public function getVersion() {
+    public function getVersion()
+    {
         return $this->version;
     }
 
-    public function setVersion($version) {
-        $this->version = $version;
+    public function setVersion($version)
+    {
+        $new = clone $this;
+        $new->version = $version;
+        return $new;
     }
 
-    public function setUserAgent($UserAgent) {
-        $this->UserAgent = $UserAgent;
+    public function setUserAgent($UserAgent)
+    {
+        $new = clone $this;
+        $new->UserAgent = $UserAgent;
+        return $new;
     }
 
-    public function setFormID($FormID) {
-        $this->FormID = $FormID;
+    public function setFormID($FormID)
+    {
+        $new = clone $this;
+        $new->FormID = $FormID;
+        return $new;
     }
 
-    public function getFormID() {
+    public function getFormID()
+    {
         return $this->FormID;
     }
 
-    public function setPublisherID($PublisherID) {
-        $this->PublisherID = $PublisherID;
+    public function setPublisherID($PublisherID)
+    {
+        $new = clone $this;
+        $new->PublisherID = $PublisherID;
+        return $new;
     }
 
-    public function setDebug($Debug) {
-        $this->Debug = $Debug;
+    public function setDebug($Debug)
+    {
+        $new = clone $this;
+        $new->Debug = $Debug;
+        return $new;
+    }
+    
+    public function getTokenInputName()
+    {
+        return $this->TokenInputName;
+    }
+    
+    public function setTokenInputName($tokenInputName)
+    {
+        $new = clone $this;
+        $new->TokenInputName = $tokenInputName;
+        return $new;
     }
 
-    public function checkFormID($FormID) {
+    public function checkFormID($FormID)
+    {
         $fields['RequestType'] = 'checkFormID';
         $fields['ResponseType'] = '2';
         $fields['FormID'] = $FormID;
@@ -84,31 +122,36 @@ class KeyPic {
         return false;
     }
 
-    public function getToken($Token, $ClientEmailAddress = '', $ClientUsername = '', 
-                                    $ClientMessage = '', $ClientFingerprint = '', $Quantity = 1) {
+    /**
+     *
+     * @param type $Token
+     * @param type $ClientEmailAddress
+     * @param type $ClientUsername
+     * @param type $ClientMessage
+     * @param type $ClientFingerprint
+     * @param type $Quantity
+     * @return boolean
+     */
+    public function getToken(
+        $Token,
+        $ClientEmailAddress = '',
+        $ClientUsername = '',
+        $ClientMessage = '',
+        $ClientFingerprint = '',
+        $Quantity = 1
+    ) {
         if ($Token) {
             $this->Token = $Token;
             return $this->Token;
         } else {
-            $serverParams = $this->request->getServerParams();
-            
-            $fields['FormID'] = $this->FormID;
-            $fields['RequestType'] = 'RequestNewToken';
-            $fields['ResponseType'] = '2';
-            $fields['Quantity'] = $Quantity;
-            $fields['ServerName'] = $serverParams['server_name'];
-            $fields['ClientIP'] = $serverParams['remote_client'];
-            $fields['ClientUserAgent'] = $this->request->getHeader('user_agent');
-            $fields['ClientAccept'] = $this->request->getHeader('accept');
-            $fields['ClientAcceptEncoding'] = $this->request->getHeader('accept_encoding');
-            $fields['ClientAcceptLanguage'] = $this->request->getHeader('accept_language');
-            $fields['ClientAcceptCharset'] = $this->request->getHeader('accept_charset');
-            $fields['ClientHttpReferer'] = $this->request->getHeader('referer');
-            $fields['ClientUsername'] = $ClientUsername;
-            $fields['ClientEmailAddress'] = $ClientEmailAddress;
-            $fields['ClientMessage'] = $ClientMessage;
-            $fields['ClientFingerprint'] = $ClientFingerprint;
-
+            $fields = $this->getWebServiceFields(
+                "RequestNewToken",
+                $ClientEmailAddress,
+                $ClientUsername,
+                $ClientFingerprint,
+                $ClientMessage,
+                $Quantity
+            );
             $response = $this->sendRequest($fields);
 
             if ($response['status'] == 'new_token') {
@@ -121,69 +164,86 @@ class KeyPic {
         return false;
     }
 
-    public function getTokenInput() {
+    /**
+     * get keypic token hidden field text
+     * @return boolean|string
+     */
+    public function getTokenInput()
+    {
         if ($this->Token) {
-            return '<input type="hidden" name="Token" value="' . $this->Token . '" />';
+            return '<input type="hidden" name="'.$this->TokenInputName.'" value="' . $this->Token . '" />';
         }
-
         return false;
     }
 
-    public function getIt($RequestType = 'getScript', $WidthHeight = '336x280') {
+    /**
+     * print keypic token into form as pixel image or script
+     * @param string $RequestType
+     * @param string $WidthHeight
+     * @return string
+     */
+    public function getIt($RequestType = 'getScript', $WidthHeight = '336x280')
+    {
         $ret = "";
         if ($this->Token) {
             switch ($RequestType) {
                 case 'getImage':
-                    $ret =  '<a href="http://' . $this->host . '/?RequestType=getClick&amp;Token=' . 
-                        $this->Token . '" target="_blank"><img src="//' . $this->host . 
-                        '/?RequestType=getImage&amp;Token=' . $this->Token . '&amp;WidthHeight=' . 
-                        $WidthHeight . '&amp;PublisherID=' . $this->PublisherID . '" alt="Form protected by Keypic" /></a>';
+                    $ret =  '<a href="http://' . $this->host . '/?RequestType=getClick&amp;Token=' .
+                        $this->Token . '" target="_blank"><img src="//' . $this->host .
+                        '/?RequestType=getImage&amp;Token=' . $this->Token . '&amp;WidthHeight=' .
+                        $WidthHeight . '&amp;PublisherID=' . $this->PublisherID .
+                        '" alt="Form protected by Keypic" /></a>';
                     break;
 
                 default:
-                    $ret =  '<script type="text/javascript" src="//' . $this->host . 
-                        '/?RequestType=getScript&amp;Token=' . $this->Token . '&amp;WidthHeight=' 
+                    $ret =  '<script type="text/javascript" src="//' . $this->host .
+                        '/?RequestType=getScript&amp;Token=' . $this->Token . '&amp;WidthHeight='
                         . $WidthHeight . '&amp;PublisherID=' . $this->PublisherID . '"></script>';
                     break;
             }
         }
 
-        if(empty($ret) === true){
+        if (empty($ret) === true) {
             $ret = '<a href="http://keypic.com" target="_blank">'.
-                    'At the moment Keypic is not properly configured in your system, '.
-                    'please check if everything is configured correctly Subscribe to the service or check your FormID</a>';
+                    'Incorrect keypic configuration</a>';
         }
         
         return $ret;
     }
 
-    // Is Spam? from 0% to 100%
-    public function isSpam($Token, $ClientEmailAddress = '', $ClientUsername = '', $ClientMessage = '', $ClientFingerprint = '') {
-        $this->Token = $Token;
+    /**
+     * Detect if entry is Spam? from 0% to 100%
+     * @param string $ClientEmailAddress
+     * @param string $ClientUsername
+     * @param string $ClientMessage
+     * @param string $ClientFingerprint
+     * @return array
+     *
+     * @todo amend to return array
+     */
+    public function isSpam(
+        $ClientEmailAddress = '',
+        $ClientUsername = '',
+        $ClientMessage = '',
+        $ClientFingerprint = ''
+    ) {
+        $this->Token = $this->request->getParsedBody()[$this->TokenInputName];
 
         if (($this->Token) && ($this->FormID)) {
+            $fields = $this->getWebServiceFields(
+                "RequestValidation",
+                $ClientUsername,
+                $ClientEmailAddress,
+                $ClientMessage,
+                $ClientFingerprint
+            );
             $fields['Token'] = $this->Token;
-            $fields['FormID'] = $this->FormID;
-            $fields['RequestType'] = 'RequestValidation';
-            $fields['ResponseType'] = '2';
-            $fields['ServerName'] = $this->request->getUri()->getHost();
-            $fields['ClientIP'] = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '';
-            $fields['ClientUserAgent'] = $this->request->getHeader('user_agent');
-            $fields['ClientAccept'] = $this->request->getHeader('accept');
-            $fields['ClientAcceptEncoding'] = $this->request->getHeader('accept_encoding');
-            $fields['ClientAcceptLanguage'] = $this->request->getHeader('accept_language');
-            $fields['ClientAcceptCharset'] = $this->request->getHeader('accept_charset');
-            $fields['ClientHttpReferer'] = $this->request->getHeader('referer');
-            $fields['ClientUsername'] = $ClientUsername;
-            $fields['ClientEmailAddress'] = $ClientEmailAddress;
-            $fields['ClientMessage'] = $ClientMessage;
-            $fields['ClientFingerprint'] = $ClientFingerprint;
 
             $response = $this->sendRequest($fields);
 
             if ($response['status'] == 'response') {
                 return $response['spam'];
-            } else if ($response['status'] == 'error') {
+            } elseif ($response['status'] == 'error') {
                 return $response['error'];
             }
         }
@@ -191,7 +251,13 @@ class KeyPic {
         return false;
     }
 
-    public function reportSpam($Token) {
+    /**
+     * sending reportSpam method to keypic webservice
+     * @return boolean|array
+     */
+    public function reportSpam()
+    {
+        $Token = $this->request->getParsedBody()[$this->TokenInputName];
         if ($Token == '') {
             return 'error';
         }
@@ -206,18 +272,28 @@ class KeyPic {
 
         $response = $this->sendRequest($fields);
         if (empty($response) !== true) {
-            return $response;
+            return json_decode($response);
         } else {
             return false;
         }
     }
 
-    private function sendRequest($fields) {
+    /**
+     * sending spam detection request to keypic servers
+     * @param  array          $fields
+     * @return boolean|array
+     */
+    private function sendRequest(array $fields)
+    {
         
-        $keypic_request = new Psr7\Request("POST", $this->host, 
-              ['content-type'=>'application/x-www-form-urlencoded',
+        $keypic_request = new Psr7\Request(
+            "POST",
+            $this->host,
+            ['content-type'=>'application/x-www-form-urlencoded',
                'User-Agent' => $this->UserAgent],
-                http_build_query($fields), '1.0');
+            http_build_query($fields),
+            '1.0'
+        );
         
         $client = new \GuzzleHttp\Client(['timeout'=>3]);
         $response = $client->send($keypic_request);
@@ -229,5 +305,46 @@ class KeyPic {
 
         return false;
     }
-
+    
+    /**
+     * getting fields used in webservice for requesting and validating keypic token
+     * @param String $RequestType
+     * @param String $ClientUsername
+     * @param String $ClientEmailAddress
+     * @param String $ClientMessage
+     * @param String $ClientFingerprint
+     * @param int $Quantity
+     * @return array
+     */
+    protected function getWebServiceFields(
+        $RequestType,
+        $ClientUsername = "",
+        $ClientEmailAddress = "",
+        $ClientMessage = "",
+        $ClientFingerprint = "",
+        $Quantity = 1
+    ) {
+        
+            $serverParams = $this->request->getServerParams();
+            $fields = [];
+            
+            $fields['FormID'] = $this->FormID;
+            $fields['RequestType'] = $RequestType;
+            $fields['ResponseType'] = '2';
+            $fields['Quantity'] = $Quantity;
+            $fields['ServerName'] = $serverParams['server_name'];
+            $fields['ClientIP'] = $serverParams['remote_client'];
+            $fields['ClientUserAgent'] = $this->request->getHeader('user_agent');
+            $fields['ClientAccept'] = $this->request->getHeader('accept');
+            $fields['ClientAcceptEncoding'] = $this->request->getHeader('accept_encoding');
+            $fields['ClientAcceptLanguage'] = $this->request->getHeader('accept_language');
+            $fields['ClientAcceptCharset'] = $this->request->getHeader('accept_charset');
+            $fields['ClientHttpReferer'] = $this->request->getHeader('referer');
+            $fields['ClientUsername'] = $ClientUsername;
+            $fields['ClientEmailAddress'] = $ClientEmailAddress;
+            $fields['ClientMessage'] = $ClientMessage;
+            $fields['ClientFingerprint'] = $ClientFingerprint;
+            
+            return $fields;
+    }
 }
